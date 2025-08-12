aa987966cd597a01f82f8d10562fc9b425962bb3
luang1t <luanzragit@gmail.com>
2025-08-12 00:40:24 -0300

    simplificando código

--- exercicios/unidade-4-lacos-de-repeticao/segundo_exercicio.py
+++ exercicios/unidade-4-lacos-de-repeticao/segundo_exercicio.py
@@ -1,8 +1,8 @@
 #Elabore um algoritmo que determine o valor de S, em que:
 #S = 1/1 – 2/4 + 3/9 – 4/16 + 5/25 – 6/36 + … - 10/100.
 soma_total = 0
-soma_pares = 0
-soma_impares = 0
+soma_pares_negativos = 0
+soma_impares_positivos = 0
 divisao = 0
 for i in range (1,11):
     a = i
@@ -10,14 +10,12 @@ for i in range (1,11):
 
     if(a % 2 != 0):
         divisao = a/b
-        resultado_impar = divisao
-        soma_impares_positivos += resultado_impar
+        soma_impares_positivos += divisao
         print(f"{a}/{b}")
     else:
-        divisao = a/b
-        resultado_par = divisao * - 1
-        soma_pares_negativos += resultado_par
+        divisao = a/b        
+        soma_pares_negativos += (divisao*-1)
         print(f"{a}/{b}")
 
-soma_total = (soma_pares + soma_impares)       
+soma_total = (soma_pares_negativos + soma_impares_positivos)       
 print(f"{soma_total}")        
